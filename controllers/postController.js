ab066b8d48ca0355f334c42bd950310d698500fd
CesareFortunato <cesare.fortunato89@gmail.com>
2026-02-09 14:38:43 +0100

    feat: create controller + functions inside controller

---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+const posts = require('../data/postsData');
+
+
+// logica index
+function index(req, res) {
+
+}
+
+// logica show
+function show(req, res) {
+    
+}
+
+
+// logica create
+function create(req, res) {
+    
+}
+
+
+// logica update
+function update(req, res) {
+    
+}
+
+
+
+// logica destroy
+function destroy(req, res) {
+    
+}
+
+module.exports = {index, show, create, update, destroy}
+
