99afe8fbec8146ebfcae142c996c83a56c0d2f8f
CesareFortunato <cesare.fortunato89@gmail.com>
2026-02-09 14:33:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//importo router
+const postsRouter = require ('./routers/posts');
+
+//importo i posts
+const posts = require('./data/postsData');
+
+app.use(express.static('public'));
+
+app.use("/posts", postsRouter)
+
+app.get('/', (req, res) => {
+    res.json(posts)
+    
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Esempio ${port}`)
+})
---
+++ routers/posts.js
@@ -0,0 +1,35 @@
+//variabile express
+const express = require('express');
+const posts = require('../data/postsData')
+
+//variabile router
+const router = express.Router();
+
+// index 
+router.get('/', function (req, res) {
+    res.send('Lista dei posts');
+});
+
+// show 
+router.get('/:id', function (req, res) {
+    res.json(posts[req.params.id])
+});
+
+// create
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
+});
+
+//update
+router.put('/:id', function (req, res) {
+    res.send('Modificare il post ' + req.params.id);
+});
+
+
+//destroy
+router.delete('/:id', function (req, res) {
+    res.send('Distruggere il post ' + req.params.id);
+});
+
+
+module.exports = router;
